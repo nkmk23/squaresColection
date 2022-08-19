ebb979457ccade6e23dda5355dc388ab2b70047f
nkmk23 <nsnicolassr@hotmail.com>
2022-08-18 19:42:50 -0500

    algoritmo de cuadros en proceso

--- sizes.js
+++ sizes.js
@@ -8,11 +8,11 @@ const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
-  animate: true
+    animate: true
 };
 
 const sketch = () => {
-  return ({ context, width, height}) => {
+    return ({ context, width, height}) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -26,24 +26,30 @@ const sketch = () => {
     const cellh = gridh / rows;
     const margx = (width - gridw) * 0.5;
     const margy = (height - gridh) * 0.5;
-    
-    for (let i = 0; i < numCells; i++){
-      const col = i % cols;
-      const row = Math.floor(i / cols);
-
-      const x = col * cellw;
-      const y = row * cellh;
-      const w = cellw;
-      const h = cellh;
-
-      context.save();
-      context.translate(margx,margy);
-      context.lineWidth = 10; 
-      context.fillStyle = "blue";
-      context.beginPath();
-      context.fillRect(x-0.025, y-0.025, w*0.95, h*0.95);
-      context.restore();
+        let counter = 0;
+        while (counter < 100) {
+            counter++;
+            //estudiar como se usa animate 
+            //crear algoritmo para los cuadros
+        for (let i = 0; i < numCells; i++) {
+            const col = i % cols;
+            const row = Math.floor(i / cols);
+            const x = col * cellw;
+            const y = row * cellh;
+            const w = cellw;
+            const h = cellh;
+            let COLOUR;
+            COLOUR = (col == 0 && row == 2) ? "red" : "blue";
+            context.save();
+            context.translate(margx, margy);
+            context.lineWidth = 10;
+            context.fillStyle = COLOUR;
+            context.beginPath();
+            context.fillRect(x - 0.025, y - 0.025, w * 0.95, h * 0.95);
+            context.restore();
+        }
     }
+    
   };
 };
 
